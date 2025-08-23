1af0ecbb5b2b5c5c1c90d1445baea5c58e00b180
Raghavendra-sb <raghavendra.sb.07@gmail.com>
2025-08-23 18:42:29 +0530

    Add login and logout routes to user routes

--- backend/source/routes/user.route.js
+++ backend/source/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,getUser } from "../controllers/user.controller.js";
+import { registerUser,getUser,loginUser,logoutUser} from "../controllers/user.controller.js";
 
 
 const router = Router();
@@ -8,6 +8,9 @@ const router = Router();
 router.route("/register").post(registerUser);
 // Route to get a user by ID
 router.route("/:id").get(getUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT,logoutUser);
+
 
 
 export default router;
